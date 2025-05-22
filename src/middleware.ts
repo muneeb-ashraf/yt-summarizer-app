d87f7227c0ea43069bc91b31a966b8ccac94eb98
Muneeb Ashraf <58017595+muneebashraf987@users.noreply.github.com>
2025-05-22 20:47:50 +0500

    stripe

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,14 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/api/webhooks/stripe',
-    '/api/webhooks/clerk',
-    '/sign-in',
-    '/sign-up',
-  ],
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
+    '/', // Run middleware on index page
+    '/(api|trpc)(.*)', // Run middleware on API routes
+  ],
 };
